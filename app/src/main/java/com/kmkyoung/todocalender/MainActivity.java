d0cb3c85c60327970ce558ddce1233b8147390ff
Andante <kmkyoung@me.com>
2014-09-26 17:12:17 +0900

    3 Fragment Create and Switching

--- app/src/main/java/com/kmkyoung/todocalender/MainActivity.java
+++ app/src/main/java/com/kmkyoung/todocalender/MainActivity.java
@@ -5,27 +5,27 @@ import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
 
 
-public class CalenderActivity extends Activity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+public class MainActivity extends Activity
+        implements NavigationDrawerFragment.NavigationDrawerCallbacks, Fragment_Calender.OnFragmentInteractionListener, Fragment_ToDoList.OnFragmentInteractionListener, Fragment_Visualization.OnFragmentInteractionListener
+{
 
     /**
      * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
      */
     private NavigationDrawerFragment mNavigationDrawerFragment;
+    private Fragment_Calender fragment_calender;
+    private Fragment_ToDoList fragment_toDolist;
+    private Fragment_Visualization fragment_visualization;
 
     /**
      * Used to store the last screen title. For use in {@link #restoreActionBar()}.
@@ -33,9 +33,14 @@ public class CalenderActivity extends Activity
     private CharSequence mTitle;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calender);
+        fragment_calender = new Fragment_Calender();
+        fragment_toDolist = new Fragment_ToDoList();
+        fragment_visualization = new Fragment_Visualization();
+
+
+        setContentView(R.layout.activity_main);
 
         mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getFragmentManager().findFragmentById(R.id.navigation_drawer);
@@ -51,23 +56,26 @@ public class CalenderActivity extends Activity
     public void onNavigationDrawerItemSelected(int position) {
         // update the main content by replacing fragments
         FragmentManager fragmentManager = getFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-                .commit();
-    }
-
-    public void onSectionAttached(int number) {
-        switch (number) {
+        switch (position)
+        {
+            case 0:
+                fragmentManager.beginTransaction()
+                        .replace(R.id.container, fragment_calender)
+                        .commit();
+                break;
             case 1:
-                mTitle = getString(R.string.title_section1);
+                fragmentManager.beginTransaction()
+                        .replace(R.id.container, fragment_toDolist)
+                        .commit();
                 break;
             case 2:
-                mTitle = getString(R.string.title_section2);
+                fragmentManager.beginTransaction()
+                        .replace(R.id.container, fragment_visualization)
+                        .commit();
                 break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
-                break;
+
         }
+
     }
 
     public void restoreActionBar() {
@@ -103,44 +111,8 @@ public class CalenderActivity extends Activity
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
+    @Override
+    public void onFragmentInteraction(Uri uri) {
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_calender, container, false);
-            return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((CalenderActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
     }
-
 }
