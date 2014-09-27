3afb74a48a6f6e31fd457303731313ff743c5afd
Andante <kmkyoung@me.com>
2014-09-28 01:04:43 +0900

    Refactoring : Unused lines

--- app/src/main/java/com/kmkyoung/todocalendar/Calendar/Fragment_Calendar.java
+++ app/src/main/java/com/kmkyoung/todocalendar/Calendar/Fragment_Calendar.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,40 +27,16 @@ import java.util.Calendar;
  *
  */
 public class Fragment_Calendar extends Fragment implements View.OnClickListener{
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-
     private Calender_GridViewAdapter calendar_gridViewAdapter;
     private GridView calendar_gridView;
     private TextView calendar_month;
     private Button calendar_pre_button, calendar_next_button;
     private ListView calendar_checklist;
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
     private int selected_year, selected_month, selected_day;
-
     private OnFragmentInteractionListener mListener;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment_Calender.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Fragment_Calendar newInstance(String param1, String param2) {
+    public static Fragment_Calendar newInstance() {
         Fragment_Calendar fragment = new Fragment_Calendar();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
     public Fragment_Calendar() {
@@ -71,10 +46,6 @@ public class Fragment_Calendar extends Fragment implements View.OnClickListener{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
 
         calendar_gridViewAdapter = new Calender_GridViewAdapter();
         calendar_gridViewAdapter.setContext(getActivity().getApplicationContext());
@@ -131,13 +102,6 @@ public class Fragment_Calendar extends Fragment implements View.OnClickListener{
         calendar_next_button = (Button)view.findViewById(R.id.calender_next_button);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
@@ -187,16 +151,7 @@ public class Fragment_Calendar extends Fragment implements View.OnClickListener{
         calendar_gridView.invalidateViews();
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         public void onFragmentInteraction(Uri uri);
