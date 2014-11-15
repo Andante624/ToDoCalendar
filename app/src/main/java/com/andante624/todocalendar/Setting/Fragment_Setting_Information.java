4ce55bb4c3a437b5e3d2af8a08fd4d0dcd42421d
Minchae Kim <minchae624@gmail.com>
2014-11-16 03:03:11 +0900

    Create : Setting Information Fragment Content

--- app/src/main/java/com/andante624/todocalendar/Setting/Fragment_Setting_Information.java
+++ app/src/main/java/com/andante624/todocalendar/Setting/Fragment_Setting_Information.java
@@ -7,45 +7,17 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.andante624.todocalendar.R;
+import com.andante624.todocalendar.Utils;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link Fragment_Setting_Information.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Fragment_Setting_Information#newInstance} factory method to
- * create an instance of this fragment.
- *
- */
 public class Fragment_Setting_Information extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private TextView versiontext, versionvalue, updatetext, updatevalue, madeby;
     private OnFragmentInteractionListener mListener;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment_Setting_Information.
-     */
-    // TODO: Rename and change types and number of parameters
     public static Fragment_Setting_Information newInstance(String param1, String param2) {
         Fragment_Setting_Information fragment = new Fragment_Setting_Information();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
     public Fragment_Setting_Information() {
@@ -55,16 +27,25 @@ public class Fragment_Setting_Information extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view =inflater.inflate(R.layout.fragment_setting_information, container, false);
+        View view = inflater.inflate(R.layout.fragment_setting_information, container, false);
+        versiontext = (TextView)view.findViewById(R.id.setting_information_versiontext);
+        versionvalue = (TextView)view.findViewById(R.id.setting_information_versionvalue);;
+        updatetext = (TextView)view.findViewById(R.id.setting_information_updatetext);;
+        updatevalue = (TextView)view.findViewById(R.id.setting_information_updatevalue);;
+        madeby = (TextView)view.findViewById(R.id.setting_information_madeby);;
+
+        versiontext.setTextColor(getActivity().getResources().getColor(Utils.getBackgroundDarkColorID(getActivity())));
+        versionvalue.setTextColor(getActivity().getResources().getColor(Utils.getBackgroundDarkColorID(getActivity())));
+        updatetext.setTextColor(getActivity().getResources().getColor(Utils.getBackgroundDarkColorID(getActivity())));
+        updatevalue.setTextColor(getActivity().getResources().getColor(Utils.getBackgroundDarkColorID(getActivity())));
+        madeby.setTextColor(getActivity().getResources().getColor(Utils.getBackgroundDarkColorID(getActivity())));
+
+
         return view;
     }
 
@@ -90,16 +71,6 @@ public class Fragment_Setting_Information extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         public void onFragmentInteraction(Uri uri);
